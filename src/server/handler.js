4f0c2c8427da2cee6a148bc1d146ffb48f44c562
faqihss <faqihsetiawan17@gmail.com>
2024-05-16 15:31:41 +0700

    change response and more in handler and storeData

--- src/server/handler.js
+++ src/server/handler.js
@@ -5,28 +5,49 @@ const storeData = require('../services/storeData');
 async function postPredictHandler(request, h) {
   const { image } = request.payload;
   const { model } = request.server.app;
-  const { confidenceScore, label, explanation, suggestion } = await predictClassification(model, image);
-  const id = crypto.randomUUID();
-  const createdAt = new Date().toISOString();
- 
-  const data = {
-    "id": id,
-    "result": label,
-    "explanation": explanation,
-    "suggestion": suggestion,
-    "confidenceScore": confidenceScore,
-    "createdAt": createdAt
+
+  // Check if image size is greater than 1MB
+  const imageSize = image._data.length;
+  const maxSize = 1000000;
+  if (imageSize > maxSize) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Payload content length greater than maximum allowed: 1000000'
+    });
+    response.code(413); // Payload Too Large
+    return response;
   }
 
-  await storeData(id, data);
- 
-  const response = h.response({
-    status: 'success',
-    message: confidenceScore > 99 ? 'Model is predicted successfully.' : 'Model is predicted successfully but under threshold. Please use the correct picture',
-    data
-  })
-  response.code(201);
-  return response;
+  try {
+    const { confidenceScore, label, explanation, suggestion } = await predictClassification(model, image);
+    const id = crypto.randomUUID();
+    const createdAt = new Date().toISOString();
+    const data = {
+      id,
+      result: label,
+      explanation,
+      suggestion,
+      confidenceScore,
+      createdAt
+    };
+    await storeData(id, data);
+
+    const message = confidenceScore > 99 ? 'Model is predicted successfully.' : 'Model is predicted successfully but under threshold. Please use the correct picture';
+    const response = h.response({
+      status: 'success',
+      message,
+      data
+    });
+    response.code(201); // Created
+    return response;
+  } catch (error) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Terjadi kesalahan dalam melakukan prediksi'
+    });
+    response.code(400); // Bad Request
+    return response;
+  }
 }
  
 module.exports = postPredictHandler;
