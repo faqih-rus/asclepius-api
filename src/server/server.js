b4109a7c1211a29464100205a982b482df9e3cfc
faqihss <faqihsetiawan17@gmail.com>
2024-05-16 12:43:53 +0700

    fix code

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
- 
 const Hapi = require('@hapi/hapi');
 const routes = require('../server/routes');
 const loadModel = require('../services/loadModel');
@@ -8,7 +7,7 @@ const InputError = require('../exceptions/InputError');
 (async () => {
     const server = Hapi.server({
         port: 3000,
-        host: 'localhost',
+        host: '0.0.0.0',
         routes: {
             cors: {
               origin: ['*'],
@@ -38,7 +37,7 @@ const InputError = require('../exceptions/InputError');
                 status: 'fail',
                 message: response.message
             })
-            newResponse.code(response.statusCode)
+            newResponse.code(response.output.statusCode)
             return newResponse;
         }
  
