679d125121f0f214e9c605d4b3c24e218e35f8bc
faqihss <faqihsetiawan17@gmail.com>
2024-05-16 20:36:04 +0700

    same(3)

--- src/services/inferenceService.js
+++ src/services/inferenceService.js
@@ -3,47 +3,44 @@ const tf = require('@tensorflow/tfjs-node');
 const { v4: uuidv4 } = require('uuid');
 const InputError = require('../exceptions/InputError');
 
-
 async function loadModelFromUrl() {
-    const modelUrl = process.env.MODEL_URL;
-    try {
-        const model = await tf.loadGraphModel(modelUrl);
-        return model;
-    } catch (error) {
-        throw new Error(`Gagal memuat model dari URL: ${error.message}`);
-    }
+  const modelUrl = process.env.MODEL_URL;
+  try {
+    const model = await tf.loadGraphModel(modelUrl);
+    return model;
+  } catch (error) {
+    throw new Error(`Gagal memuat model dari URL: ${error.message}`);
+  }
 }
 
 async function predictClassification(image) {
-    try {
-        const model = await loadModelFromUrl();
+  try {
+    const model = await loadModelFromUrl();
+    const tensor = tf.node
+      .decodeJpeg(image)
+      .resizeNearestNeighbor([224, 224])
+      .expandDims(0)
+      .toFloat();
+    const prediction = model.predict(tensor);
+    const [classResult] = prediction.squeeze().dataSync();
+    const label = classResult === 1 ? "Cancer" : "Non-cancer";
+    const suggestion = label === "Cancer" ? "Segera periksa ke dokter!" : "Tidak terdeteksi kanker.";
+    const id = uuidv4();
+    const createdAt = new Date().toISOString();
 
-        const tensor = tf.node
-            .decodeJpeg(image)
-            .resizeNearestNeighbor([224, 224])
-            .expandDims()
-            .toFloat();
-        
-        const prediction = model.predict(tensor);
-        const classResult = tf.argMax(prediction, 1).dataSync()[0];
-        const label = classResult === 1 ? "Cancer" : "Non-cancer";
-        const suggestion = label === "Cancer" ? "Segera periksa ke dokter!" : "Tidak terdeteksi kanker.";
-        const id = uuidv4();
-        const createdAt = new Date().toISOString();
-
-        return {
-            status: "success",
-            message: "Model is predicted successfully",
-            data: {
-                id,
-                result: label,
-                suggestion,
-                createdAt
-            }
-        };
-    } catch (error) {
-        throw new InputError(`Terjadi kesalahan input: ${error.message}`);
-    }
+    return {
+      status: "success",
+      message: "Model is predicted successfully",
+      data: {
+        id,
+        result: label,
+        suggestion,
+        createdAt
+      }
+    };
+  } catch (error) {
+    throw new InputError(`Terjadi kesalahan input: ${error.message}`);
+  }
 }
 
 module.exports = predictClassification;
