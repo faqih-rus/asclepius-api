de53dea591172656433f0a9892169ab756bfb7c1
faqihss <faqihsetiawan17@gmail.com>
2024-05-16 13:36:58 +0700

    fix 41 line code

--- src/server/server.js
+++ src/server/server.js
@@ -22,27 +22,28 @@ const InputError = require('../exceptions/InputError');
  
     server.ext('onPreResponse', function (request, h) {
         const response = request.response;
- 
         if (response instanceof InputError) {
-            const newResponse = h.response({
-                status: 'fail',
-                message: `${response.message} Silakan gunakan foto lain.`
-            })
-            newResponse.code(response.statusCode)
-            return newResponse;
+          const newResponse = h.response({
+            status: 'fail',
+            message: `${response.message} Silakan gunakan foto lain.`
+          });
+          // Convert statusCode to an integer
+          const statusCode = parseInt(response.statusCode, 10);
+          newResponse.code(statusCode);
+          return newResponse;
         }
- 
         if (response.isBoom) {
-            const newResponse = h.response({
-                status: 'fail',
-                message: response.message
-            })
-            newResponse.code(response.output.statusCode)
-            return newResponse;
+          const newResponse = h.response({
+            status: 'fail',
+            message: response.message
+          });
+          // Convert statusCode to an integer
+          const statusCode = parseInt(response.output.statusCode, 10);
+          newResponse.code(statusCode);
+          return newResponse;
         }
- 
         return h.continue;
-    });
+      });
  
     await server.start();
     console.log(`Server start at: ${server.info.uri}`);
