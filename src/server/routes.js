30235d124fa7f33fd83c7c9ca766b2e3419f07e7
faqih-rus <60426295+faqih-rus@users.noreply.github.com>
2024-05-19 06:55:15 +0700

    Update routes.js

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,18 +1,23 @@
-const { postPredictHandler } = require('./handler');
+const { postPredictHandler, getHistoriesHandler } = require('./handler');
 
 const routes = [
-    {
-        method: 'POST',
-        path: '/predict',
-        handler: postPredictHandler,
-        options: {
-            payload: {
-                output: 'stream',
-                parse: true,
-                allow: 'multipart/form-data'
-            }
-        }
-    }
+  {
+    method: 'POST',
+    path: '/predict',
+    handler: postPredictHandler,
+    options: {
+      payload: {
+        output: 'stream',
+        parse: true,
+        allow: 'multipart/form-data',
+      },
+    },
+  },
+  {
+    method: 'GET',
+    path: '/predict/histories', 
+    handler: getHistoriesHandler, 
+  },
 ];
 
 module.exports = routes;
