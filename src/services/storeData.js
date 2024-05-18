5e2f2d7cb0973fa2845c92e6d6b9bc4481efb7f7
faqih-rus <60426295+faqih-rus@users.noreply.github.com>
2024-05-19 06:57:00 +0700

    Update storeData.js

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -1,10 +1,20 @@
 const { Firestore } = require('@google-cloud/firestore');
- 
+const { GoogleAuth } = require('google-auth-library');
+
+const auth = new GoogleAuth({
+  credentials: require('./submissionmlgc-muhammadfaqihs-3f650124c82b.json'),
+  scopes: ['https://www.googleapis.com/auth/datastore'],
+});
+
 async function storeData(id, data) {
-  const db = new Firestore();
- 
+  // Authenticate with Firestore
+  const db = new Firestore({
+    projectId: 'submissionmlgc-muhammadfaqihs',
+    auth: auth, // Provide authentication credentials
+  });
+
   const predictCollection = db.collection('predictions');
   return predictCollection.doc(id).set(data);
 }
- 
-module.exports = storeData;
+
+module.exports = { storeData };
